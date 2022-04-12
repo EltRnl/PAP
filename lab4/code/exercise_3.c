f3a7bced836ecdc1fe9e27c9dbb1008d9d7d158b
Elliot Renel <elliotrenel@gmail.com>
2022-04-12 11:20:24 +0200

    exo 3 finished

--- lab4/code/exercise_3.c
+++ lab4/code/exercise_3.c
@@ -33,7 +33,7 @@ void lbm_comm_init_ex3(lbm_comm_t * comm, int total_width, int total_height)
 void lbm_comm_ghost_exchange_ex3(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
 	//
-	// TODO: Implement the 1D communication with non-blocking MPI functions.
+	// DONE: Implement the 1D communication with non-blocking MPI functions.
 	//
 	// To be used:
 	//    - DIRECTIONS: the number of doubles composing a cell
@@ -44,4 +44,22 @@ void lbm_comm_ghost_exchange_ex3(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	//example to access cell
 	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
 	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+
+	double* send_left = lbm_mesh_get_cell(mesh,1,0);
+	double* recv_left = lbm_mesh_get_cell(mesh,0,0);
+
+	double* send_right = lbm_mesh_get_cell(mesh,comm->width-2,0);
+	double* recv_right = lbm_mesh_get_cell(mesh,comm->width-1,0);
+
+	MPI_Request request[4];
+	int count = 0;
+	
+	if(comm->rank_x!=0) MPI_Irecv(recv_left,DIRECTIONS*comm->height,MPI_DOUBLE,comm->rank_x-1,0,MPI_COMM_WORLD,request+(count++));
+	if(comm->rank_x!=comm->nb_x-1) MPI_Isend(send_right,DIRECTIONS*comm->height,MPI_DOUBLE,comm->rank_x+1,0,MPI_COMM_WORLD,request+(count++));
+
+	if(comm->rank_x!=comm->nb_x-1) MPI_Irecv(recv_right,DIRECTIONS*comm->height,MPI_DOUBLE,comm->rank_x+1,0,MPI_COMM_WORLD,request+(count++));
+	if(comm->rank_x!=0) MPI_Isend(send_left,DIRECTIONS*comm->height,MPI_DOUBLE,comm->rank_x-1,0,MPI_COMM_WORLD,request+(count++));
+
+	MPI_Waitall(count,request,MPI_STATUS_IGNORE);
+
 }
